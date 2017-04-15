6360c5dc562458cee56cc15ce55e563162587125
Alan <kurfew007@gmail.com>
2017-04-15 12:23:45 -0400

    Create board module with CheckersBoard class

---
+++ source/board.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""board
+
+This module provides access to the CheckersBoard class. This class is used by
+the checkers simulation for a checkers playing robot.
+
+Author: Alan Grant
+Version: 1.0
+Date: 04/12/2017
+Class: CSCI-C 458
+"""
+import sys
+
+class CheckersBoard:
+    """CheckersBoard
+    
+    The CheckersBoard class represents a checkers board. It is a simple class
+    for storing the layout of a checkers board. It provides an easy way to
+    print the board by using the magic string method.
+    
+    Attributes:
+        layout (str) : The file path to file containing the board layout.\n
+        board (list) : A list of strings which represents the layout of the
+        board. 
+    """
+    
+    def __init__(self, layout):
+        """__init__
+        
+        The __init__ method is the constructor for the CheckersBoard class. It
+        attempts to open the layout file and read in the board layout to
+        initialize the board.
+        
+        Args:
+            layout (str) : The file path to the layout file. 
+        """
+        self.layout = layout
+        self.reset()
+
+
+    
+
